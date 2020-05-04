7bed09924905357731c247be2de0e59f3fbfc486
dreyWesson <dreyWesson@gmail.com>
2020-05-04 15:12:34 +0100

    refactored the regex check

--- regex.js
+++ regex.js
@@ -4,9 +4,10 @@ const email = document.getElementById("email");
 const password1 = document.getElementById("password1");
 const password2 = document.getElementById("password2");
 
-let usernameValue, emailValue, password1Value, password2Value;
-let emailPattern = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-let passwordPattern = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
+// const usernameValue = username.value.trim();
+// const emailValue = email.value.trim();
+// const password1Value = password1.value.trim();
+// const password2Value = password2.value.trim();
 
 username.addEventListener("blur", validateUsername);
 email.addEventListener("blur", validateEmail);
@@ -42,9 +43,8 @@ function validateAllOnSubmit() {
 }
 
 function validateUsername() {
-  usernameValue = username.value.trim();
-
-  if (usernameValue == "") setErrorFor(username, "Username cannot be blank");
+  if (username.value.trim() == "")
+    setErrorFor(username, "Username cannot be blank");
   else {
     setSuccessFor(username);
     return true;
@@ -52,10 +52,8 @@ function validateUsername() {
 }
 
 function validateEmail() {
-  emailValue = email.value.trim();
-
-  if (emailValue === "") setErrorFor(email, "Email can't be blank");
-  else if (!regexValidator(emailPattern, emailValue))
+  if (email.value.trim() === "") setErrorFor(email, "Email can't be blank");
+  else if (!isEmail(email.value.trim()))
     setErrorFor(email, "Enter a valid email");
   else {
     setSuccessFor(email);
@@ -64,10 +62,9 @@ function validateEmail() {
 }
 
 function validatePassword1() {
-  password1Value = password1.value.trim();
-
-  if (password1Value == "") setErrorFor(password1, "Password cannot be blank");
-  else if (!regexValidator(passwordPattern, password1Value))
+  if (password1.value.trim() == "")
+    setErrorFor(password1, "Password cannot be blank");
+  else if (!isPassword(password1.value.trim()))
     setErrorFor(password1, "Your password is too weak");
   else {
     setSuccessFor(password1);
@@ -76,23 +73,16 @@ function validatePassword1() {
 }
 
 function validatePassword2() {
-  password1Value = password1.value.trim();
-  password2Value = password2.value.trim();
-
-  if (password2Value == "") setErrorFor(password2, "Password cannot be blank");
-  else if (!regexValidator(passwordPattern, password2Value))
+  if (password2.value.trim() == "")
+    setErrorFor(password2, "Password cannot be blank");
+  else if (!isPassword(password2.value.trim()))
     setErrorFor(password2, "Your password is too weak");
-  else if (password2Value != password1Value)
+  else if (password2.value.trim() != password1.value.trim())
     setErrorFor(password2, "Password doesn't match");
   else {
     setSuccessFor(password2);
     return true;
   }
-}
-
-function regexValidator(pattern, input) {
-  console.log(pattern.test(input));
-  return pattern.test(input);
 }
 
 function setErrorFor(input, message) {
@@ -108,4 +98,13 @@ function setSuccessFor(input) {
   const formControl = input.parentElement; //.form-control
   // add success class
   formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  let regex = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+  return regex.test(email);
+}
+function isPassword(password1) {
+  let regex = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
+  return regex.test(password1);
 }
